1c58a0aaf346338eafc5d0d036ea2c046a04b6f0
Daniel Peppicelli <daniel.peppicelli@epfl.ch>
2015-11-26 17:24:50 +0100

    Added translation for comments link

--- js/overlay.js
+++ js/overlay.js
@@ -11,16 +11,16 @@
             player.playVideo();
         }
 
-        window.displayFlickrGallery = function(flickrGalleryID, title, link) {
+        window.displayFlickrGallery = function(flickrGalleryID, title, link, link_title) {
             $("#overlay-title").html(title);
-            $("#overlay-link").html('<a href="' + link + '">Comments</a>');
+            $("#overlay-link").html('<a href="' + link + '">' + link_title + '</a>');
             $(".overlay-container").html("<div class='overlay-container-flickr'><iframe id='iframe' src='//flickrit.com/slideshowholder.php?height=75&size=big&setId=" + flickrGalleryID + "&caption=on&credit=1&thumbnails=0&transition=0&layoutType=responsive&sort=0' scrolling='no' frameborder='0'style='width:100%; height:100%; position: absolute; top:0; left:0;' ></iframe></div>");
             $(".overlay").removeClass('overlay-hugeinc').addClass('overlay-hugeinc-open overlay-hugeinc');
         };
 
-        window.displayVideo = function(youTubeURL, title, link) {
+        window.displayVideo = function(youTubeURL, title, link, link_title) {
             $("#overlay-title").html(title);
-            $("#overlay-link").html('<a href="' + link + '">Comments</a>');
+            $("#overlay-link").html('<a href="' + link + '">' + link_title + '</a>');
             $(".overlay-container").html('<iframe width="560" id="video-player" height="315" src="' + youTubeURL + '?enablejsapi=1&showinfo=0" frameborder="0" autoplay="1"></iframe>');
             // Lazy load youtube API if not yet available
             if(typeof YT === "object") {
